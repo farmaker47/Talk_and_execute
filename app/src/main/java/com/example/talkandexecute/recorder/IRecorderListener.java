b55ad62398b8cdf2d80c56cc1d31e0e5a2b674a0
Georgios <farmaker47@gmail.com>
2023-12-25 12:57:06 +0200

    Clean and convert WaveUtil.java to kotlin.

--- app/src/main/java/com/example/talkandexecute/recorder/IRecorderListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.talkandexecute.recorder;
-
-public interface IRecorderListener {
-    void onUpdateReceived(String message);
-
-    void onDataReceived(float[] samples);
-}
--- app/src/main/java/com/example/talkandexecute/utils/WaveUtil.kt
+++ app/src/main/java/com/example/talkandexecute/utils/WaveUtil.kt
@@ -1,137 +1,157 @@
-package com.example.talkandexecute.utils;
+package com.example.talkandexecute.utils
 
-import android.util.Log;
+import android.util.Log
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.charset.StandardCharsets
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.charset.StandardCharsets;
-
-public class WaveUtil {
-    public static final String TAG = "WaveUtil";
-    public static final String RECORDING_FILE = "MicInput.wav";
-
-    public static void createWaveFile(String filePath, byte[] samples, int sampleRate, int numChannels, int bytesPerSample) {
+object WaveUtil {
+    const val TAG = "WaveUtil"
+    fun createWaveFile(
+        filePath: String?,
+        samples: ByteArray,
+        sampleRate: Int,
+        numChannels: Int,
+        bytesPerSample: Int
+    ) {
         try {
-            int dataSize = samples.length; // actual data size in bytes
-            int audioFormat = (bytesPerSample == 2) ? 1 : (bytesPerSample == 4) ? 3 : 0; // PCM_16 = 1, PCM_FLOAT = 3
-
-            FileOutputStream fileOutputStream = new FileOutputStream(filePath);
-            fileOutputStream.write("RIFF".getBytes(StandardCharsets.UTF_8)); // Write the "RIFF" chunk descriptor
-            fileOutputStream.write(intToByteArray(36 + dataSize), 0, 4); // Total file size - 8 bytes
-            fileOutputStream.write("WAVE".getBytes(StandardCharsets.UTF_8)); // Write the "WAVE" format
-            fileOutputStream.write("fmt ".getBytes(StandardCharsets.UTF_8)); // Write the "fmt " sub-chunk
-            fileOutputStream.write(intToByteArray(16), 0, 4); // Sub-chunk size (16 for PCM)
-            fileOutputStream.write(shortToByteArray((short) audioFormat), 0, 2); // Audio format (1 for PCM)
-            fileOutputStream.write(shortToByteArray((short) numChannels), 0, 2); // Number of channels
-            fileOutputStream.write(intToByteArray(sampleRate), 0, 4); // Sample rate
-            fileOutputStream.write(intToByteArray(sampleRate * numChannels * bytesPerSample), 0, 4); // Byte rate
-            fileOutputStream.write(shortToByteArray((short) (numChannels * bytesPerSample)), 0, 2); // Block align
-            fileOutputStream.write(shortToByteArray((short) (bytesPerSample * 8)), 0, 2); // Bits per sample
-            fileOutputStream.write("data".getBytes(StandardCharsets.UTF_8)); // Write the "data" sub-chunk
-            fileOutputStream.write(intToByteArray(dataSize), 0, 4); // Data size
+            val dataSize = samples.size // actual data size in bytes
+            val audioFormat =
+                if (bytesPerSample == 2) 1 else if (bytesPerSample == 4) 3 else 0 // PCM_16 = 1, PCM_FLOAT = 3
+            val fileOutputStream = FileOutputStream(filePath)
+            fileOutputStream.write("RIFF".toByteArray(StandardCharsets.UTF_8)) // Write the "RIFF" chunk descriptor
+            fileOutputStream.write(intToByteArray(36 + dataSize), 0, 4) // Total file size - 8 bytes
+            fileOutputStream.write("WAVE".toByteArray(StandardCharsets.UTF_8)) // Write the "WAVE" format
+            fileOutputStream.write("fmt ".toByteArray(StandardCharsets.UTF_8)) // Write the "fmt " sub-chunk
+            fileOutputStream.write(intToByteArray(16), 0, 4) // Sub-chunk size (16 for PCM)
+            fileOutputStream.write(
+                shortToByteArray(audioFormat.toShort().toInt()),
+                0,
+                2
+            ) // Audio format (1 for PCM)
+            fileOutputStream.write(
+                shortToByteArray(numChannels.toShort().toInt()),
+                0,
+                2
+            ) // Number of channels
+            fileOutputStream.write(intToByteArray(sampleRate), 0, 4) // Sample rate
+            fileOutputStream.write(
+                intToByteArray(sampleRate * numChannels * bytesPerSample),
+                0,
+                4
+            ) // Byte rate
+            fileOutputStream.write(
+                shortToByteArray(
+                    (numChannels * bytesPerSample).toShort().toInt()
+                ), 0, 2
+            ) // Block align
+            fileOutputStream.write(
+                shortToByteArray((bytesPerSample * 8).toShort().toInt()),
+                0,
+                2
+            ) // Bits per sample
+            fileOutputStream.write("data".toByteArray(StandardCharsets.UTF_8)) // Write the "data" sub-chunk
+            fileOutputStream.write(intToByteArray(dataSize), 0, 4) // Data size
 
             // Write audio samples
-            fileOutputStream.write(samples);
+            fileOutputStream.write(samples)
 
             // Close the file output stream
-            fileOutputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.e(TAG, "Error...", e);
+            fileOutputStream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Log.e(TAG, "Error...", e)
         }
     }
 
-    public static float[] getSamples(String filePath) {
+    @JvmStatic
+    fun getSamples(filePath: String?): FloatArray {
         try {
-            FileInputStream fileInputStream = new FileInputStream(filePath);
+            val fileInputStream = FileInputStream(filePath)
 
             // Read the WAV file header
-            byte[] header = new byte[44];
-            fileInputStream.read(header);
+            val header = ByteArray(44)
+            fileInputStream.read(header)
 
             // Check if it's a valid WAV file (contains "RIFF" and "WAVE" markers)
-            String headerStr = new String(header, 0, 4);
-            if (!headerStr.equals("RIFF")) {
-                System.err.println("Not a valid WAV file");
-                return new float[0];
+            val headerStr = String(header, 0, 4)
+            if (headerStr != "RIFF") {
+                System.err.println("Not a valid WAV file")
+                return FloatArray(0)
             }
 
             // Get the audio format details from the header
-            int sampleRate = byteArrayToNumber(header, 24, 4);
-            int bitsPerSample = byteArrayToNumber(header, 34, 2);
+            val sampleRate = byteArrayToNumber(header, 24, 4)
+            val bitsPerSample = byteArrayToNumber(header, 34, 2)
             if (bitsPerSample != 16 && bitsPerSample != 32) {
-                System.err.println("Unsupported bits per sample: " + bitsPerSample);
-                return new float[0];
+                System.err.println("Unsupported bits per sample: $bitsPerSample")
+                return FloatArray(0)
             }
 
             // Get the size of the data section (all PCM data)
-            int dataLength = fileInputStream.available(); // byteArrayToInt(header, 40, 4);
+            val dataLength = fileInputStream.available() // byteArrayToInt(header, 40, 4);
 
             // Calculate the number of samples
-            int bytesPerSample = bitsPerSample / 8;
-            int numSamples = dataLength / bytesPerSample;
+            val bytesPerSample = bitsPerSample / 8
+            val numSamples = dataLength / bytesPerSample
 
             // Read the audio data
-            byte[] audioData = new byte[dataLength];
-            fileInputStream.read(audioData);
-            ByteBuffer byteBuffer = ByteBuffer.wrap(audioData);
-            byteBuffer.order(ByteOrder.nativeOrder());
+            val audioData = ByteArray(dataLength)
+            fileInputStream.read(audioData)
+            val byteBuffer = ByteBuffer.wrap(audioData)
+            byteBuffer.order(ByteOrder.nativeOrder())
 
             // Convert audio data to PCM_FLOAT format
-            float[] samples = new float[numSamples];
+            val samples = FloatArray(numSamples)
             if (bitsPerSample == 16) {
-                for (int i = 0; i < numSamples; i++) {
-                    samples[i] = (float) (byteBuffer.getShort() / 32768.0);
+                for (i in 0 until numSamples) {
+                    samples[i] = (byteBuffer.getShort() / 32768.0).toFloat()
                 }
             } else if (bitsPerSample == 32) {
-                for (int i = 0; i < numSamples; i++) {
-                    samples[i] = byteBuffer.getFloat();
+                for (i in 0 until numSamples) {
+                    samples[i] = byteBuffer.getFloat()
                 }
             }
-
-            return samples;
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.e(TAG, "Error...", e);
+            return samples
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Log.e(TAG, "Error...", e)
         }
-        return new float[0];
+        return FloatArray(0)
     }
 
     // Convert a portion of a byte array into an integer or a short
-    private static int byteArrayToNumber(byte[] bytes, int offset, int length) {
-        int value = 0; // Start with an initial value of 0
+    private fun byteArrayToNumber(bytes: ByteArray, offset: Int, length: Int): Int {
+        var value = 0 // Start with an initial value of 0
 
         // Loop through the specified portion of the byte array
-        for (int i = 0; i < length; i++) {
+        for (i in 0 until length) {
             // Extract a byte, ensure it's positive, and shift it to its position in the integer
-            value |= (bytes[offset + i] & 0xFF) << (8 * i);
+            value = value or (bytes[offset + i].toInt() and 0xFF shl 8 * i)
         }
-
-        return value; // Return the resulting integer value
+        return value // Return the resulting integer value
     }
 
-    private static byte[] intToByteArray(int value) {
-        byte[] byteArray = new byte[4]; // Create a 4-byte array
+    private fun intToByteArray(value: Int): ByteArray {
+        val byteArray = ByteArray(4) // Create a 4-byte array
 
         // Convert and store the bytes in little-endian order
-        byteArray[0] = (byte) (value & 0xFF);         // Least significant byte (LSB)
-        byteArray[1] = (byte) ((value >> 8) & 0xFF);  // Second least significant byte
-        byteArray[2] = (byte) ((value >> 16) & 0xFF); // Second most significant byte
-        byteArray[3] = (byte) ((value >> 24) & 0xFF); // Most significant byte (MSB)
-
-        return byteArray;
+        byteArray[0] = (value and 0xFF).toByte() // Least significant byte (LSB)
+        byteArray[1] = (value shr 8 and 0xFF).toByte() // Second least significant byte
+        byteArray[2] = (value shr 16 and 0xFF).toByte() // Second most significant byte
+        byteArray[3] = (value shr 24 and 0xFF).toByte() // Most significant byte (MSB)
+        return byteArray
     }
 
-    private static byte[] shortToByteArray(int value) {
-        byte[] byteArray = new byte[2]; // Create a 2-byte array
+    private fun shortToByteArray(value: Int): ByteArray {
+        val byteArray = ByteArray(2) // Create a 2-byte array
 
         // Convert and store the bytes in little-endian order
-        byteArray[0] = (byte) (value & 0xFF);        // Least significant byte (LSB)
-        byteArray[1] = (byte) ((value >> 8) & 0xFF); // Most significant byte (MSB)
-
-        return byteArray;
+        byteArray[0] = (value and 0xFF).toByte() // Least significant byte (LSB)
+        byteArray[1] = (value shr 8 and 0xFF).toByte() // Most significant byte (MSB)
+        return byteArray
     }
 }
